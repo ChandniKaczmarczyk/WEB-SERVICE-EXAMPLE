634fdaf409404fb1edf9127d4ff04db27511e497
Chandnipatel57 <chandnip057@gmail.com>
2017-06-02 23:14:04 -0500

    Create WebService Application and add  method for addition of two numbers

---
+++ WebServiceExample1/WebServiceExample1/WebServiceCalculator.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceExample1
+{
+    /// <summary>
+    /// Summary description for WebServiceCalculator
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceCalculator : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int AddTwoNumbers(int num1, int num2)
+        {
+            var num3 = num1 + num2;
+            return num3;
+        }
+    }
+}
